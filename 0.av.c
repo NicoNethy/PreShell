26e83ae894b984fe10238ad6c2c4d654265bc9ff
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-29 09:33:23 -0800

    Abe

---
+++ 0.av.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Commit
+ * @ac: ac
+ * @av: av
+ * Return: 0
+ */
+int main(int ac, char **av)
+{
+	char x = 1;
+
+	while (av[x] != NULL)
+	{
+		printf("%s\n", av[x]);
+
+		av[x++];
+	}
+	return (0);
+}
