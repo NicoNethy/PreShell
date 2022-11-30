32fbed6ee1dfe0ce32cdbebf343dd3c142a06fc6
lucassoriabusto <lucassoriabusto@hotmail.com>
2022-11-30 10:51:08 -0800

    1.Readline

--- 1.Readline.c
+++ 1.Readline.c
@@ -6,15 +6,18 @@
  */
 int main()
 {
-    char buffer[32];
-    char *b = buffer;
-    size_t bufsize = 32;
-    size_t characters;
+	char buffer[32];
+	char *b = buffer;
+	size_t bufsize = 32;
+	size_t characters;
 
-    printf("$ ");
-    characters = getline(&b,&bufsize,stdin);
-    printf("%zu characters were read.\n",characters);
-    printf("You typed:'%s'\n",buffer);
-
-    return(0);
+	while (1 != EOF)
+	{
+		printf("$ ");
+		characters = getline(&b,&bufsize,stdin);
+		printf("%s",buffer);
+		if (characters == -1)
+			return (-1);
+	}
+	return(0);
 }
