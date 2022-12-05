d3b5432958a00f59d90d80cec03418bab55fa899
root <5761@holbertonstudents.com>
2022-12-05 10:50:56 -0800

    asd

---
+++ TestN1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+/**
+ *
+ */
+int main()
+{
+	char *buff = NULL;
+	char *b = NULL;
+	size_t bufsize = 32;
+	size_t characters;
+	char *argv[] = {"/bin/ls", "/usr/", NULL};
+	//char * token = strtok(buffer, " ");
+
+	while (1 != EOF)
+	{
+		printf("$ ");
+		getline(&b,&bufsize,stdin);
+
+		buff = strtok(b, "\n");
+		argv[1] = NULL;
+
+		printf("buff = %s\n", buff);
+		if (strcmp(buff, "/bin/ls") == 0)
+		{
+			printf("entra aca xd\n");
+			execve(buff, argv, NULL);
+		}
+		else
+			printf("error\n");
+	}
+	return(0);
+}
---
+++ supersimple.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/**
+ *  *
+ *   */
+
+
+
+
+
+
+
+
+int main()
+{
+	char buffer[32];
+	char *b = buffer;
+	size_t bufsize = 32
+	size_t characters;
+
+	while (1 != EOF)
+	{
+		printf("$ ");
+		characters = getline(&b,&bufsize,stdin);
+		printf("%s",buffer);
+		if (characters == -1)
+			return (-1);
+	}
+	return(0);
+}
